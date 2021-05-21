85f12e7ab39066ab12db0f4627ef221b1de34907
defstiller <81063003+defstiller@users.noreply.github.com>
2021-05-21 08:32:56 -0500

    Changes in crypto js calculations, styles for crypto

--- resources/crypto.js
+++ resources/crypto.js
@@ -8,24 +8,29 @@ fetch("https://api.coingecko.com/api/v3/coins/safelight?tickers=false&market_dat
         let safeLightPrice = parseFloat(responseData.market_data.current_price.usd).toFixed(9); // Prevents conversion to scientific formula
         let innerLightMessage = document.getElementById("safeLight");
         let lightProfit = document.getElementById("safeLightProfit")
-        var lightInvestedAt = 0.00000001;
-        if (safeLightPrice == 1e-9) { //Price is not displayed right on API side, even if to convert it to number it will show only 0.00000001, and not show anything after
+        var lightInvestedAt = 0.000000006;
+        let lightDiv = document.getElementById("lightDiv")
+        if (safeLightPrice <= 1e-9) { //Price is not displayed right on API side, even if to convert it to number it will show only 0.00000001, and not show anything after
             innerLightMessage.innerHTML = "SafeLight current price is " + safeLightPrice;
             lightProfit.innerHTML = "Price is still too low to calculate profit";
         }
         else if (safeLightPrice == lightInvestedAt) {
-            innerLightMessage.innerHTML = "SafeLight current price is " + safeLightPrice;
+            innerLightMessage.innerHTML = "SafeLight current price is $" + safeLightPrice;
             lightProfit.innerHTML = "No profit"
         }
         else {
-            innerLightMessage.innerHTML = "SafeLight current price is " + safeLightPrice;
+            innerLightMessage.innerHTML = "SafeLight current price is $" + safeLightPrice;
 
             if (safeLightPrice > lightInvestedAt) {
             let profitLightCalculator = Math.floor((safeLightPrice / lightInvestedAt - 1) *100);
-            lightProfit.innerHTML = "My profit is " + profitLightCalculator + " %"} 
+            lightProfit.innerHTML = "My profit is " + profitLightCalculator + " %";
+            lightDiv.style.backgroundColor = "green";
+            } 
+            
             else {
             let lossLightCalculator = Math.floor((lightInvestedAt / safeLightPrice - 1) *100);
             lightProfit.innerHTML = "My loss is " + lossLightCalculator + " %";
+            lightDiv.style.backgroundColor = "red";
             }
         }
     })
@@ -39,23 +44,27 @@ fetch("https://api.coingecko.com/api/v3/coins/safebtc?tickers=false&market_data=
     .then(responseData => {
         let safeBTCPrice = parseFloat(responseData.market_data.current_price.usd).toFixed(9); // Prevents conversion to scientific formula
         let innerBTCMessage = document.getElementById("safeBTC");
-        var BTCInvestedAt = 0.00000011;
+        var BTCInvestedAt = 0.00000012;
         let safeBTCProf = document.getElementById("safeBTCProfit")
-        if (safeBTCPrice == 1e-8 || safeBTCPrice == 1e-9) { //Price is not displayed right on API side, even if to convert it to number it will show only 0.00000001, and not show anything after
+        let btcDiv = document.getElementById("btcDiv")
+        if (safeBTCPrice <= 1e-9) { //Price is not displayed right on API side, even if to convert it to number it will show only 0.00000001, and not show anything after
             innerBTCMessage.innerHTML = "Price is still too low to calculate";
         }
         else if (safeBTCPrice == BTCInvestedAt) {
             safeBTCProf.innerHTML = "No profit"
         }
         else {
-        innerBTCMessage.innerHTML = "SafeBTC current price is " + safeBTCPrice;
+        innerBTCMessage.innerHTML = "SafeBTC current price is $" + safeBTCPrice;
 
             if (safeBTCPrice > BTCInvestedAt) {
             let profitBTCCalculator = Math.floor((safeBTCPrice / BTCInvestedAt) *100);
-            safeBTCProf.innerHTML = "My profit is " + profitBTCCalculator + " %"}
+            safeBTCProf.innerHTML = "My profit is " + profitBTCCalculator + " %";
+            btcDiv.style.backgroundColor = "green";
+            }
             else {
             let lossBTCCalculator = Math.floor((BTCInvestedAt / safeBTCPrice - 1)*100);
-            safeBTCProf.innerHTML = "My loss is " + lossBTCCalculator + " %"
+            safeBTCProf.innerHTML = "My loss is " + lossBTCCalculator + " %";
+            btcDiv.style.backgroundColor = "red";
             }
         }
 
